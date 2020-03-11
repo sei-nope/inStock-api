1df1ae67d5b6fed58eff9bed8725796d34b63fcc
kurotsubasa <kurotsubasa10@gmail.com>
2020-03-11 10:15:11 -0400

    Create and populate inventory model
    Create inventory model with name, quantity, and price

---
+++ app/models/inventory.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const inventorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+  // owner: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'User',
+  //   required: true
+  // }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Inventory', inventorySchema)
